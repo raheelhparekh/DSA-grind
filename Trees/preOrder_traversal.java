cb5fd0a11abf63dabc383b9ce942d2a120ac0390
raheelhparekh <rhparekh2003@gmail.com>
2024-07-18 17:10:51 +0530

    Iterative Version Preorder tarversal in trees using stacks

--- Trees/preOrder_traversal.java
+++ Trees/preOrder_traversal.java
@@ -24,3 +24,37 @@ public class preOrder_traversal {
         
     }
 }
+
+/*
+ * Iterative Version using Stacks
+ * 
+ * // stack me root dala initially
+    // root ko pop kiya usko ans me dala
+    // root ka right agar he toh stack me add kiya
+    // root ka left agar he toh usko add kiya
+ * 
+ * 
+ * class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        if(root==null) return ans;
+
+        Stack<TreeNode> st=new Stack<>();
+        st.push(root);
+
+        while(!st.isEmpty()){
+            root=st.pop();
+            ans.add(root.val);
+
+            if(root.right!=null){
+                st.push(root.right);
+            }
+            if(root.left!=null){
+                st.push(root.left);
+            }
+            
+        }
+        return ans;
+    }
+}
+ */
