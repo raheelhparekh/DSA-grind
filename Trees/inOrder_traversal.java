bcadda6112e69eee25a2174fb7e902f39cdddbac
raheelhparekh <rhparekh2003@gmail.com>
2024-07-28 18:23:28 +0530

    Iterative Inorder Traversal

--- Trees/inOrder_traversal.java
+++ Trees/inOrder_traversal.java
@@ -25,3 +25,28 @@ public class inOrder_traversal {
         inorder(root.right);
     }
 }
+
+/*
+ * public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        Stack<TreeNode> stack=new Stack<>();
+
+        TreeNode node=root;
+        while(true){
+            if(node!=null){
+                stack.push(node);
+                node=node.left;
+            }
+            else{
+                if(stack.isEmpty()){
+                    break;
+                }
+                node=stack.pop();
+                list.add(node.val);
+                node=node.right;
+
+            }
+        }
+        return list;
+    }
+ */
