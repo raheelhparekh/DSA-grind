8594aedc8d8af59a0e0587194d947ae77bf95d12
raheelhparekh <rhparekh2003@gmail.com>
2024-07-29 17:54:22 +0530

    Leet Prob 2149 : Rearrange Array Elements by Sign

--- Arrays/alternate_positive_negative.java
+++ Arrays/alternate_positive_negative.java
@@ -1,58 +1,38 @@
 package Arrays;
 /*
- * GeekForGeeks: Alternate positive and negative numbers
- * Link: https://practice.geeksforgeeks.org/problems/array-of-alternate-ve-and-ve-nos1401/1
- * Input: 
-        N = 9
-        Arr[] = {9, 4, -2, -1, 5, 0, -5, -3, 2}
-        Output:
-        9 -2 4 -1 5 -5 0 -3 2
-        Explanation : Positive elements : 9,4,5,0,2
-        Negative elements : -2,-1,-5,-3
-        As we need to maintain the relative order of
-        postive elements and negative elements we will pick
-        each element from the positive and negative and will
-        store them. If any of the positive and negative numbers
-        are completed. we will continue with the remaining signed
-        elements.The output is 9,-2,4,-1,5,-5,0,-3,2.
-    
-    TC: O(n)
-    SC: O(n)
-    Solved by me --> Yes but reffered to GFG for the last part of solution for the if condition
+ * LeetCode 2149. Rearrange Array Elements by Sign
+ * Link:https://leetcode.com/problems/rearrange-array-elements-by-sign/
+ * TC: O(n)
+ * SC: O(n)
  */
 
-import java.util.ArrayList;
-
 public class alternate_positive_negative {
-     void rearrange(int arr[], int n) {
-        // code here
-        ArrayList<Integer> positiveList=new ArrayList<>();
-        ArrayList<Integer> negativeList=new ArrayList<>();
-        
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>=0){
-                positiveList.add(arr[i]);
-            }
-            else{
-                negativeList.add(arr[i]);
+    public int[] rearrangeArray(int[] nums) {
+        int pos[] = new int[nums.length / 2];
+        int posIndex = 0;
+        int neg[] = new int[nums.length / 2];
+        int negIndex = 0;
+        int ans[] = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                pos[posIndex++] = nums[i];
+            } else {
+                neg[negIndex++] = nums[i];
             }
         }
-        int arr_index=0;
-        int positiveList_index=0;
-        int negativeList_index=0;
-        
-        
-        for(int i=0;i<n;i++){
-            if(i<positiveList.size()){
-                arr[arr_index++]=positiveList.get(positiveList_index++);
+
+        int i = 0;
+        int j = 0;
+        int index = 0;
+
+        for (int x = 0; x < nums.length; x++) {
+            if (x % 2 == 0) {
+                ans[index++] = pos[i++];
+            } else {
+                ans[index++] = neg[j++];
             }
-            if(i<negativeList.size()){
-                arr[arr_index++]=negativeList.get(negativeList_index++);
-            }
-           
         }
-        
+        return ans;
     }
-    
-    
 }
