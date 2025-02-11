32978c8e3f9f6397fe6a2329c653f4d47c6da23a
raheelhparekh <rhparekh2003@gmail.com>
2025-02-11 21:46:28 +0530

    Post Order Traversal using 2 Stack

--- Trees/postOrder.java
+++ Trees/postOrder.java
@@ -25,3 +25,34 @@ public class postOrder {
         
     }
 }
+
+/*
+ * using 2 stacks
+ * 
+ * public List<Integer> postorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack1=new Stack<>();
+        Stack<TreeNode> stack2=new Stack<>();
+        List<Integer> ans =new ArrayList<>();
+        
+        if(root==null) return ans;
+
+        stack1.push(root);
+        while(!stack1.isEmpty()){
+            root=stack1.pop();
+            stack2.push(root);
+
+            if(root.left!=null){
+                stack1.push(root.left);
+            }
+            if(root.right!=null){
+                stack1.push(root.right);
+            }
+        }
+
+        while(!stack2.isEmpty()){
+            int valueOfNode=stack2.pop().val;
+            ans.add(valueOfNode);
+        }
+        return ans;
+    }
+ */
