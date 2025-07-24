ea3ca20c45c738f6a5d7894ece6b22a137be580e
raheelhparekh <rhparekh2003@gmail.com>
2025-07-24 18:29:58 +0530

    Leet Prob 21 : Merge 2 Sorted linked list

--- LinkedLists/merge_two_LL.java
+++ LinkedLists/merge_two_LL.java
@@ -4,40 +4,45 @@ package LinkedLists;
  *  Link: https://leetcode.com/problems/merge-two-sorted-lists/
  *  TC : 0(N+M)
  *  SC : 0(1)
+ * 
+ * DUMMY NODE
+ * here we are just re arranging the links of the existing nodes
+ * so we don't need to create new linked list
+ * dummy node is used to avoid confusion and simply return dummy.next
  *  
  */
 
 public class merge_two_LL {
     class Solution {
         public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-            ListNode ans=new ListNode();
-            ListNode head=ans;
-    
-            while(list1!=null && list2!=null){
-                if(list1.val<list2.val){
-                    head.next=list1;
-                    list1=list1.next;
-                    head=head.next;
-                    
+
+            ListNode t1 = list1;
+            ListNode t2 = list2;
+
+            ListNode dummy = new ListNode(-1);
+            ListNode curr = dummy;
+
+            while (t1 != null && t2 != null) {
+                if (t1.val < t2.val) {
+                    curr.next = t1;
+                    t1 = t1.next;
+                    curr = curr.next;
+                } else {
+                    curr.next = t2;
+                    t2 = t2.next;
+                    curr = curr.next;
                 }
-                else{
-                    head.next=list2;
-                    list2=list2.next;
-                    head=head.next;
-                }
             }
-            while(list1!=null){
-                head.next=list1;
-                list1=list1.next;
-                head=head.next;
+
+            if (t1 != null) {
+                curr.next = t1;
             }
-            while(list2!=null){
-                head.next=list2;
-                list2=list2.next;
-                head=head.next;
+            if (t2 != null) {
+                curr.next = t2;
             }
-            return ans.next;
+
+            return dummy.next;
+
         }
     }
-    
 }
