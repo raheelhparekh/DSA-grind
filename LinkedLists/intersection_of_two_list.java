758d1d6f7aeff88cfe955f84213f72415a4cd381
raheelhparekh <rhparekh2003@gmail.com>
2025-07-26 13:03:10 +0530

    Leet Prob 160. Intersection of Two Linked Lists {HARD}

--- LinkedLists/intersection_of_two_list.java
+++ LinkedLists/intersection_of_two_list.java
@@ -11,39 +11,76 @@ package LinkedLists;
 
 public class intersection_of_two_list {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA == null || headB==null){
+        if (headA == null || headB == null) {
             return null;
         }
 
-        int lenA=0;
-        ListNode curr=headA;
-        while(curr!=null){
-            curr=curr.next;
+        int lenA = 0;
+        ListNode curr = headA;
+        while (curr != null) {
+            curr = curr.next;
             lenA++;
         }
-        int lenB=0;
-        curr=headB;
-        while(curr!=null){
-            curr=curr.next;
+        int lenB = 0;
+        curr = headB;
+        while (curr != null) {
+            curr = curr.next;
             lenB++;
         }
 
-        while(lenA>lenB){
-            headA=headA.next;
+        while (lenA > lenB) {
+            headA = headA.next;
             lenA--;
         }
-        while(lenB>lenA){
-            headB=headB.next;
+        while (lenB > lenA) {
+            headB = headB.next;
             lenB--;
         }
 
-        while(headA!=headB){
-            headA=headA.next;
-            headB=headB.next;
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
         }
 
         return headA;
 
-        
     }
 }
+
+
+/*
+ *MOST OPTIMAL APPROACH
+    * TC : O(N+M)
+    * SC : O(1)
+    STRIVERS A TO Z
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode t1 = headA;
+        ListNode t2 = headB;
+
+        if (t1 == t2)
+            return t1;
+
+        while (t1 != t2) {
+
+            t1 = t1.next;
+            t2 = t2.next;
+
+            if (t1 == t2) {
+                return t1;
+            }
+
+            if (t1 == null) {
+                t1 = headB;
+
+            }
+            if (t2 == null) {
+                t2 = headA;
+
+            }
+        }
+
+        return t1;
+
+    }
+ */
